3026e2501014d166c15c4410f1c766ebc0eef52c
Yash Shinde <yashshinde337@gmail.com>
2023-12-25 17:42:53 +0530

    added full reddit clone

--- index.js
+++ index.js
@@ -12,7 +12,6 @@ themeToggler.addEventListener("click", () => {
     themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
 })
 
-
 function openAppPopup() {
     appPopup.classList.add("open-appPopup");
     main.classList.add("blur-filter");
@@ -41,3 +40,39 @@ function login() {
     loginPopup.classList.remove('open-loginPopup');
     main.classList.remove("blur-filter");
 }
+
+function fetchData() {
+    fetch('https://dummyapi.io/data/v1/post', {
+        method: "GET",
+        headers: {
+            'Content-type': 'application/json',
+            'app-id': '6589295b844d7d66af764dcb'
+        }
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.data)
+            for (let i = 0; i < data.data.length; i++) {
+                main.innerHTML +=
+                    `<div class="post">
+                        <div class="top">
+                            <img src=${data.data[i].owner.picture} alt="img">
+                            <p><b>r/${data.data[i].owner.firstName}  ${data.data[i].owner.lastName}</b></p>
+                        </div>
+                        <div class="info">
+                            <p>${data.data[i].text}</p>
+                            <img src="${data.data[i].image}" alt="img">
+                        </div>
+                        <div class="bottom">
+                            <button><span class="material-symbols-sharp">favorite</span>${data.data[i].likes}</button>
+                            <button><span class="material-symbols-sharp">chat_bubble</span>450</button>
+                            <button><span class="material-symbols-sharp">send</span>Share</button>
+                        </div>
+                    </div>
+                    <hr>`
+            }
+        })
+        .catch(error => console.log(error));
+}
+
+fetchData();
